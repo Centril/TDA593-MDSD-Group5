f079fcbc14e98f30e780504a3e63c005be3f9c27
Mazdak Farrokhzad <twingoow@gmail.com>
2015-12-26 13:21:59 +0100

    Added IsSearchCriteria helper

---
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/search/logic/IsSearchCriteria.java
@@ -0,0 +1,12 @@
+package sechalmersmdsdgroup5.hotel.search.logic;
+
+import sechalmersmdsdgroup5.hotel.search.SearchCriteria;
+import sechalmersmdsdgroup5.hotel.search.impl.MatchesSearchCriteriaImpl;
+
+import java.util.function.Function;
+
+public class IsSearchCriteria<SRT, IN> extends MatchesSearchCriteriaImpl<SRT, IN> implements SearchCriteria<SRT> {
+	IsSearchCriteria( IN input, Function<SRT, IN> transform ) {
+		super( input, transform, (l, r) -> l == r );
+	}
+}
