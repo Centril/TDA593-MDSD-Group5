863a53d5261baea236b11866e3e1a82c12c0a559
Mazdak Farrokhzad <twingoow@gmail.com>
2015-12-27 03:16:29 +0100

    Fix #125 - added SearchCriteriaFactory with most basic operations - todo: add more related to actual domain entities.

---
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/search/logic/SearchCriteriaFactory.java
@@ -0,0 +1,43 @@
+package sechalmersmdsdgroup5.hotel.search.logic;
+
+import sechalmersmdsdgroup5.hotel.search.SearchCriteria;
+import sechalmersmdsdgroup5.hotel.search.impl.AndSearchCriteriaImpl;
+import sechalmersmdsdgroup5.hotel.search.impl.MatchesSearchCriteriaImpl;
+import sechalmersmdsdgroup5.hotel.search.impl.NotSearchCriteriaImpl;
+import sechalmersmdsdgroup5.hotel.search.impl.OrSearchCriteriaImpl;
+import sechalmersmdsdgroup5.hotel.utils.Functional;
+
+import java.util.Arrays;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
+public class SearchCriteriaFactory {
+	public static <SRT> SearchCriteria<SRT> id() {
+		return result -> result;
+	}
+
+	@SafeVarargs
+	public static <SRT> SearchCriteria<SRT> and( SearchCriteria<SRT> l, SearchCriteria<SRT> r,
+	                                             SearchCriteria<SRT>... cs ) {
+		return Functional.foldl( Arrays.asList( cs ), new AndSearchCriteriaImpl<>( l, r ), AndSearchCriteriaImpl::new );
+	}
+
+	@SafeVarargs
+	public static <SRT> SearchCriteria<SRT> or( SearchCriteria<SRT> l, SearchCriteria<SRT> r,
+	                                            SearchCriteria<SRT>... cs ) {
+		return Functional.foldl( Arrays.asList( cs ), new OrSearchCriteriaImpl<>( l, r ), OrSearchCriteriaImpl::new );
+	}
+
+	public static <SRT> SearchCriteria<SRT> not( SearchCriteria<SRT> c ) {
+		return new NotSearchCriteriaImpl<>( c );
+	}
+
+	public static <SRT, IN> SearchCriteria<SRT> matches( IN input, Function<SRT, IN> transform,
+	                                                     BiPredicate<IN, IN> predicate ) {
+		return new MatchesSearchCriteriaImpl<>( input, transform, predicate );
+	}
+
+	public static <SRT, IN> SearchCriteria<SRT> eq( IN input, Function<SRT, IN> transform ) {
+		return new IsSearchCriteria<>( input, transform );
+	}
+}
