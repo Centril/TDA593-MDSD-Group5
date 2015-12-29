ee8dff40e9286060c03a177f14234ed398e82e21
Mazdak Farrokhzad <twingoow@gmail.com>
2015-12-29 23:48:15 +0100

    added hotel.cli.Main, CLI program, empty shell.

---
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/cli/Main.java
@@ -0,0 +1,14 @@
+package sechalmersmdsdgroup5.hotel.cli;
+
+/**
+ * CLI program.
+ */
+public class Main {
+	/**
+	 * CLI entry point.
+	 *
+	 * @param args CLI args.
+	 */
+	public static void main( String[] args ) {
+	}
+}
