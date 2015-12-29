c25a0c34ccd6d1e791392e13f1b18867d33c6d3f
Kalior <kalioragus@gmail.com>
2015-12-29 18:57:44 +0100

    Add empty constructor to CheckInCheckOut

--- application/src/main/java/sechalmersmdsdgroup5/hotel/ordering/impl/CheckInCheckOut.java
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/ordering/impl/CheckInCheckOut.java
@@ -14,6 +14,9 @@ import java.util.Date;
  * Created by kalior on 18/12/15.
  */
 public class CheckInCheckOut implements ICheckInCheckOut {
+
+    public CheckInCheckOut() {}
+
     @Override
     public boolean checkIn(Guest guest, RoomBooking booking) {
         /*
