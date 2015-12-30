6aaa3ba3bc51a79f16a6b9b35e86d16dba666743
Alotte <alma.ottedag@gmail.com>
2015-12-30 18:07:20 +0100

    fixed Debit prematurely, will merge and change some stuff.

--- application/src/main/java/sechalmersmdsdgroup5/hotel/payment/impl/Payment.java
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/payment/impl/Payment.java
@@ -1,6 +1,8 @@
 package sechalmersmdsdgroup5.hotel.payment.impl;
 
 import sechalmersmdsdgroup5.hotel.clients.Client;
+import sechalmersmdsdgroup5.hotel.ordering.Invoice;
+import sechalmersmdsdgroup5.hotel.ordering.OrderingFactory;
 import sechalmersmdsdgroup5.hotel.payment.CreditCard;
 import sechalmersmdsdgroup5.hotel.payment.IPayment;
 import sechalmersmdsdgroup5.hotel.payment.Payable;
@@ -16,13 +18,51 @@ public class Payment implements IPayment{
         return true;
     }
 
+    /**
+     * Client pays directly through the machine.
+     * @param payable
+     */
     @Override
-    public void debit(Payable invoicable) {
-
+    public void debit(Payable payable) {
+        //Here the customer pays externally using a card reader,
+        //such that the payable's total cost is covered.
+        payable.setIsPaid(true);
     }
 
+    /**
+     * Here the client is debitted through an invoice.
+     * @param payable anything which a client has to pay for.
+     * @param client
+     */
     @Override
-    public void debit(Payable invoicable, Client customer) {
+    public void debit(Payable payable, Client client) {
+        if ( payable.isPaid() ) {
+            return;
+        }
+
+        if ( payable.getInvoice() == null || payable.getInvoice().isEmpty() ) {
+            // Create invoice if needed.
+            Invoice invoice = OrderingFactory.INSTANCE.createInvoice();
+           /*
+            TODO fix this when merged changes from issue #131 and #128
+            invoice.setTotalPrice(payable.totalPrice());
+            client.addInvoice(invoice);
+            // The client will then get a mail or physical copy of the invoice which he or she will pay for.
+            */
+            //This is outside the system and therefore the invoice will simply be regarded as paid here.
+            invoice.setIsPaid(true);
+            payable.setIsPaid(true);
+        }
+        else {
+            // Flag all non-paid invoices as paid and assign to client.
+            for (Invoice invoice : payable.getInvoice() ) {
+                if ( !invoice.isPaid() ) {
+                    client.addInvoice(invoice);
+                    invoice.setIsPaid(true);
+                }
+            }
+            //Assume the client will pay outside the system.
+        }
 
     }
 
