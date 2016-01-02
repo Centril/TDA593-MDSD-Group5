70acdf322eadc2fb8d31e2f67eb908549a28af74
Mazdak Farrokhzad <twingoow@gmail.com>
2016-01-02 01:08:56 +0100

    fixing #133, CLI: removed old attempt at CLI.

--- application/src/main/java/sechalmersmdsdgroup5/hotel/cli/model/Application.java
+++
@@ -1,7 +0,0 @@
-package sechalmersmdsdgroup5.hotel.cli.model;
-
-import java.util.Map;
-import java.util.function.Supplier;
-
-public interface Application<I> extends Supplier<Map<String, Command<I, ?>>> {
-}
