652399158eb09688d760693b615f11c9bfd981ba
Mazdak Farrokhzad <twingoow@gmail.com>
2016-01-02 06:39:28 +0100

    fixed #133, CLI: using infrastructure in Main with some examples.

--- application/src/main/java/sechalmersmdsdgroup5/hotel/cli/App.java
+++
@@ -1,7 +0,0 @@
-package sechalmersmdsdgroup5.hotel.cli;
-
-class App implements Runnable {
-	@Override
-	public void run() {
-	}
-}
--- application/src/main/java/sechalmersmdsdgroup5/hotel/cli/Main.java
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/cli/Main.java
@@ -1,5 +1,18 @@
 package sechalmersmdsdgroup5.hotel.cli;
 
+import sechalmersmdsdgroup5.hotel.Hotel;
+import sechalmersmdsdgroup5.hotel.cli.infrastructure.Application;
+import sechalmersmdsdgroup5.hotel.cli.infrastructure.color.TermColor;
+
+import java.util.Arrays;
+
+import static sechalmersmdsdgroup5.hotel.HotelFactory.INSTANCE;
+import static sechalmersmdsdgroup5.hotel.cli.infrastructure.ApplicationRunner.runner;
+import static sechalmersmdsdgroup5.hotel.cli.infrastructure.Command.command;
+import static sechalmersmdsdgroup5.hotel.cli.infrastructure.IOHelper.system;
+import static sechalmersmdsdgroup5.hotel.cli.infrastructure.Readers.reader;
+import static sechalmersmdsdgroup5.hotel.cli.infrastructure.color.StandardPrintColor.GREEN;
+
 /**
  * CLI program.
  */
@@ -10,6 +23,36 @@ public class Main {
 	 * @param args CLI args.
 	 */
 	public static void main( String[] args ) {
-		new App().run();
+		checkDisableColors( args );
+
+		runner( system(), INSTANCE.createHotel() ).accept( Application.<Hotel>builder()
+			.cmdVoid( "error", "tests error", (io, $) -> io.error( "hello world" ) )
+			.cmdVoid( "hello", "displays hello world", (io, $) -> io.info( "hello world" ) )
+			.cmdVoid( "green", "displays hello world in green", (io, $) -> io.newline( GREEN, "hello world" ) )
+			.cmdVoid( "test", "stuff", (io, $) ->
+				io.cancelable().info( "size: " + io.readMany( 2, "label:", reader(
+					x -> x.startsWith( "a" ) ) ).size() ) )
+			.cmdVoid( "sub", "runs subcommands", (io, $) ->
+				io.cancelable()
+				  .info( "size: " + io.executeMany( 2, $, command( "",
+						(io2, $2) -> io2.cancelable().read( "aaa" ) ) ).size() )
+				  .info( "1337" ) )
+			.cmdVoid( "sub2", "runs subcommands", (io, $) ->
+				io.cancelable()
+				  .info( "size: " + io.executeMany( 2, $, command( "", (io2, $2) -> io2.read( "aaa" ) ) ).size() )
+				  .info( "1337" ) )
+			.build() );
+	}
+
+	/**
+	 * Disable terminal colors if instructed to.
+	 *
+	 * @param args CLI args.
+	 */
+	private static void checkDisableColors( String[] args ) {
+		if ( Arrays.stream( args ).map( String::trim ).anyMatch(
+				str -> str.equals( "--no-color" ) || str.equals( "-nc" ) ) ) {
+			TermColor.enabled = false;
+		}
 	}
 }
