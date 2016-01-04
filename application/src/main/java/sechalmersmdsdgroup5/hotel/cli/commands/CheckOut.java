03453885619a0ee13004b50fc0cf71af80132099
Kalior <kalioragus@gmail.com>
2016-01-04 17:48:36 +0100

    Add class CheckOut in cli

---
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/cli/commands/CheckOut.java
@@ -0,0 +1,26 @@
+package sechalmersmdsdgroup5.hotel.cli.commands;
+
+import sechalmersmdsdgroup5.hotel.Hotel;
+import sechalmersmdsdgroup5.hotel.cli.infrastructure.Command;
+import sechalmersmdsdgroup5.hotel.cli.infrastructure.IOHelper;
+import sechalmersmdsdgroup5.hotel.cli.infrastructure.IdentifiableCommand;
+
+/**
+ * Created by kalior on 04/01/16.
+ */
+public class CheckOut implements Command.Consuming<Hotel>, IdentifiableCommand<Hotel, Void> {
+    @Override
+    public void accept(IOHelper ioHelper, Hotel hotel) {
+        
+    }
+
+    @Override
+    public String getIdentifier() {
+        return null;
+    }
+
+    @Override
+    public String help() {
+        return null;
+    }
+}
