357fe8267aaa8e6a4bc7907ebbfcc03c4ad39926
Mazdak Farrokhzad <twingoow@gmail.com>
2016-01-04 03:22:48 +0100

    refactored Utils.readDate + added readOrTomorrow

--- application/src/main/java/sechalmersmdsdgroup5/hotel/cli/commands/Utils.java
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/cli/commands/Utils.java
@@ -3,14 +3,26 @@ package sechalmersmdsdgroup5.hotel.cli.commands;
 import sechalmersmdsdgroup5.hotel.cli.infrastructure.IOHelper;
 import sechalmersmdsdgroup5.hotel.cli.readers.StandardReaders;
 
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.function.Supplier;
+
+import static sechalmersmdsdgroup5.hotel.utils.Dates.toDate;
 
 /**
  * Utils for commands.
  */
 public class Utils {
+	public static Date readOrTomorrow( String label, IOHelper io ) {
+		return readDate( label, io, () -> toDate( LocalDateTime.now().plusDays( 1 ) ) );
+	}
+
 	public static Date readDate( String label, IOHelper io ) {
-		return io.readOpt( label, StandardReaders.date() ).orElseGet( Date::new );
+		return readDate( label, io, Date::new );
+	}
+
+	public static Date readDate( String label, IOHelper io, Supplier<Date> whenInvalid ) {
+		return io.readOpt( label, StandardReaders.date() ).orElseGet( whenInvalid );
 	}
 
 	public static Integer readInteger( String label, IOHelper io ) {
