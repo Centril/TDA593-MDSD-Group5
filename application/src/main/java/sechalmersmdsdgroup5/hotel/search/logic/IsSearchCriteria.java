f44ae8d850bd41165a54971004309157aaba8cec
Mazdak Farrokhzad <twingoow@gmail.com>
2016-01-04 07:05:05 +0100

    IsSearchCriteria: made ctor public.

--- application/src/main/java/sechalmersmdsdgroup5/hotel/search/logic/IsSearchCriteria.java
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/search/logic/IsSearchCriteria.java
@@ -6,7 +6,7 @@ import sechalmersmdsdgroup5.hotel.search.impl.MatchesSearchCriteriaImpl;
 import java.util.function.Function;
 
 public class IsSearchCriteria<SRT, IN> extends MatchesSearchCriteriaImpl<SRT, IN> implements SearchCriteria<SRT> {
-	IsSearchCriteria( IN input, Function<SRT, IN> transform ) {
+	public IsSearchCriteria( IN input, Function<SRT, IN> transform ) {
 		super( input, transform, IN::equals );
 	}
 }
