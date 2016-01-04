e7f91195c2923eeecfd97c4dac9b483e6da13a26
Mattias Nilsen <mattias.m.nilsen@gmail.com>
2016-01-04 16:36:17 +0100

    Implemented getBlacklistReason

--- application/src/main/java/sechalmersmdsdgroup5/hotel/blacklist/impl/IBlacklistImpl.java
+++ application/src/main/java/sechalmersmdsdgroup5/hotel/blacklist/impl/IBlacklistImpl.java
@@ -6,6 +6,8 @@ import sechalmersmdsdgroup5.hotel.blacklist.IBlacklist;
 import sechalmersmdsdgroup5.hotel.identities.Identity;
 
 import java.util.Date;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Created by Kerp on 31/12/15.
@@ -30,6 +32,17 @@ public class IBlacklistImpl implements IBlacklist {
 
     @Override
     public void removeFromBlackList(Identity identity) {
-        hotel.getBlacklist().getItems().removeIf((item) -> item.getIdentity().getIdNumber() == identity.getIdNumber());
+        hotel.getBlacklist().getItems().removeIf((item) -> item.getIdentity().getIdNumber().equals(identity.getIdNumber()));
+    }
+
+    @Override
+    public String getBlacklistReason(Identity identity) {
+        BlacklistItem blacklistItem = hotel.getBlacklist().getItems().stream().filter((item)
+                                                -> item.getIdentity().getIdNumber().equals(identity.getIdNumber())).findFirst().get();
+
+        if(blacklistItem != null) {
+            return blacklistItem.getReason();
+        }
+        return null;
     }
 }
